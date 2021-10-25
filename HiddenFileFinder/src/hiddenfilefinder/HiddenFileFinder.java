c3878a4b200f0cf8c3f0814049a3262558ecc334
Shubham Singh Rathore <shubhamsinghrathore13@gmail.com>
2021-10-26 02:32:01 +0530

    Add files via upload

---
+++ HiddenFileFinder/src/hiddenfilefinder/HiddenFileFinder.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hiddenfilefinder;
+
+/**
+ *
+ * @author Shubham Gupta
+ */
+public class HiddenFileFinder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       NewJFrame t1=new NewJFrame();
+       t1.setVisible(true);
+    }
+    
+}
